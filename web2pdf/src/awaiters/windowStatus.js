65ddccd503b3ba6a8ae1310709d41d3866289fb2
Oleg Novik-Dichko <oleg.novikdichko@gmail.com>
2017-11-15 00:31:42 +0300

    Fix window status awaiter client code

--- web2pdf/src/awaiters/windowStatus.js
+++ web2pdf/src/awaiters/windowStatus.js
@@ -1,20 +1,22 @@
 const awaiterCtr = (status, timeout = 60 * 1000) => async client => {
-    const browserCode = 
-    `return new Promise((fulfill, reject) => {
-        const checkinterval = 50;
-        let ctr = 0;
-        const intervalId = setInterval(() => {
-            if (window.status === "${status}") {
-                clearInterval(intervalId);
-                fulfill();
-            }
-            if (ctr >= ${timeout}) {
-                clearInterval(intervalId);
-                reject();
-            }
-            ctr += checkinterval;
-        }, checkinterval);
-    });`
+    const browserCode =`
+        (() => {
+            return new Promise((fulfill, reject) => {
+                const checkinterval = 50;
+                let ctr = 0;
+                const intervalId = setInterval(() => {
+                    if (window.status === "${status}") {
+                        clearInterval(intervalId);
+                        fulfill();
+                    }
+                    if (ctr >= ${timeout}) {
+                        clearInterval(intervalId);
+                        reject();
+                    }
+                    ctr += checkinterval;
+                }, checkinterval);
+            });
+        })()`
 
     await client.Runtime.evaluate({
         expression: browserCode,
